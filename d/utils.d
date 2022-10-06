cfae2b3d0ecbba45cd62f61b268a37dea99edf15
YamaD <me@dyama.net>
2022-10-07 00:08:57 +0900

    a

--- d/utils.d
+++ d/utils.d
@@ -1,3 +1,3 @@
 auto avg(T)(T[] a) => a.fold!`a+b` / real(a.length);
 auto toa(T, U)(U[] a) => a[].map!(to!T).array;
-auto pop(T)(auto ref T[] a) { T v=a.front; a.popFront; return v; }
+auto pop(T)(auto ref T[] a) { auto v=a.front; a.popFront; return v; }
