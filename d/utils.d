06379708f43f6d5ddab3fab2d88adcf7487b26df
YamaD <me@dyama.net>
2022-10-12 07:51:08 +0900

    add timestamp

--- d/utils.d
+++ d/utils.d
@@ -1,3 +1,7 @@
 auto avg(T)(T[] a) => a.fold!`a+b` / real(a.length);
 auto toa(T, U)(U[] a) => a[].map!(to!T).array;
 auto pop(T)(auto ref T[] a) { auto v=a.front; a.popFront; return v; }
+
+auto timestamp(SysTime st) => (st.toUTC - SysTime.fromUnixTime(0)).total!"msecs";
+auto timestamp(DateTime dt) => dt.SysTime.timestamp;
+auto timestamp() => Clock.currTime.timestamp;
