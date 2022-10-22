05ce47f95662d1b0e871d569e4b19a6f455a64c5
YamaD <me@dyama.net>
2022-10-22 20:16:12 +0900

    update dyns

--- d/dyns.d
+++ d/dyns.d
@@ -2,87 +2,160 @@
 
 import std;
 
-alias _Dyns(_...) = void[0];
-
-class Dyns {
-  alias FieldTup = Tuple!(string, "key", size_t, "size");
-
-  struct Field {
-    Dyns self;
-    const size_t i;
-    auto ptr() const => self.ptr(i);
-    auto ref val(T)() => *cast(T*)ptr;
-    auto ref size() => self.sizes[i];
-    auto toString() const => self.ptr(i).to!string;
-  }
-
-  size_t[string] keys;
-  size_t[] sizes;
-  void* buf;
-
-  this() {}
-  this(const Dyns d) {
-    keys = cast(size_t[string])d.keys; //
-    sizes = d.sizes.dup;
-  }
-  this(FieldTup[] a) { a.each!(t => register(t)); }
-  this(void* p) { opAssign(p); }
-
-  auto idx(string key) const => keys[key];
-  auto offset(size_t i) const => i ? sizes[0..i].sum : 0;
-  auto offset(string key) const => offset(idx(key));
-  auto ptr(T)(T key) const => buf + offset(key);
-
-  void register(string key, size_t size) {
-    keys[key] = keys.length;
-    sizes ~= size;
-  }
-  void register(FieldTup t) => register(t.key, t.size);
-  void register(T)(string key, size_t len=1) => register(key, T.sizeof * len);
-
-  auto size() const => sizes.sum;
-  void alloc() { buf = new ubyte[size].ptr; }
-
-  auto opAssign(void* p) => buf = p;
-  auto opDispatch(string key)() => Field(this, idx(key));
-
-  static auto from(T...)() {
-    auto kv(U, string key, V...)() {
-      // static ifじゃないとkv!Vの型が評価されちゃう
-      static if(V.length) return [FieldTup(key, U.sizeof)] ~ kv!V;
-      else                return [FieldTup(key, U.sizeof)];
+class Dyns(T...) {
+  static if(T.length == 1) {
+    // T is a struct
+    alias Types = Fields!T;
+  } else {
+    // T are fields
+    template Keys(T, string _, U...) {
+      static if(U.length) alias Keys = AliasSeq!(T, Keys!U);
+      else                alias Keys = T;
     }
-    enum tups = kv!T; // ctfe
-    return new Dyns(tups);
+    alias Types = Keys!T;
   }
 
-  static auto from(T)() if(is(T == struct)) {
-    enum tups = (){
-      FieldTup[] a;
-      static foreach(field; [__traits(derivedMembers, T)])
-        a ~= FieldTup(field, mixin(`T.` ~ field).sizeof);
-      return a;
-    }(); // ctfe
-    return new Dyns(tups);
+  alias Size = SumType!(size_t, size_t delegate());
+
+  enum isDyns(T) = __traits(isSame, TemplateOf!Dyns, TemplateOf!T);
+
+  static init() {
+    size_t[string] idx;
+    typeof(_dyns_sizes) sizes;
+    static if(T.length == 1) {
+      // T is a struct
+      static foreach(key; [__traits(derivedMembers, T)]) {{
+        alias I = typeof(mixin(`T[0].` ~ key));
+        idx[key] = idx.length;
+        static if(isDyns!I) Size s = 0;
+        else                Size s = I.sizeof;
+        sizes ~= s;
+      }}
+    } else {
+      // T are fields
+      auto unpack(U, string key, V...)() {
+        idx[key] = idx.length;
+        static if(isDyns!U) Size s = 0;
+        else                Size s = U.sizeof;
+        sizes ~= s;
+        static if(V.length) unpack!V;
+      }
+      unpack!T;
+    }
+    return tuple!("idx", "sizes")(idx, sizes);
+  }
+
+ private:
+  enum size_t[string] _dyns_idx = init.idx;
+  Size[] _dyns_sizes = init.sizes;
+  SumType!Types[size_t] _dyns_dynses;
+  void* _dyns_buf;
+
+ public:
+  this() {
+    static foreach(i, Type; Types) static if(isDyns!Type) {
+      _dyns_dynses[i] = new Type(ptr(i));
+      _dyns_sizes[i] = () => _dyns_dynses[i].tryMatch!((Type d) => d.size);
+    }
+  }
+  this(const void* p) {
+    this();
+    opAssign(p);
+  }
+
+  auto offset(size_t i) const {
+    if(!i) return 0;
+    return _dyns_sizes[0..i].map!(s => s.match!(
+        (size_t v) => v,
+        (size_t delegate() f) => f()
+      )).sum;
+  }
+  auto offset(string key) const => offset(_dyns_idx[key]);
+  auto ptr(U)(U field) const => _dyns_buf + offset(field);
+
+  // field
+  auto ref getSize(size_t i) => _dyns_sizes[i].match!(
+      (ref size_t v) => v,
+      (ref size_t delegate() f) => f()
+    );
+  auto ref setSize(size_t i, size_t n) => _dyns_sizes[i].match!(
+      (ref size_t v) => v = n,
+      (ref size_t delegate() f) => f()
+    );
+
+  // total
+  auto size() const => _dyns_sizes.map!(s => s.match!(
+      (size_t v) => v,
+      (size_t delegate() f) => f()
+    )).sum;
+  void alloc() { opAssign(new ubyte[size].ptr); }
+
+  auto opAssign(const void* p) {
+    _dyns_buf = cast(void*)p;
+    return this;
+  }
+
+  auto opDispatch(string key)() if(_dyns_idx.keys.canFind(key)) {
+    enum i = _dyns_idx[key];
+    alias Type = Types[i];
+
+    struct Field {
+      Dyns self;
+      auto ptr() const => self.ptr(i);
+      auto ref size() => self.getSize(i);
+      auto ref size(size_t n) => self.setSize(i, n);
+      auto ref val() {
+        static if(isArray!Type) return cast(ElementType!Type*)ptr;
+        else                    return *cast(Type*)ptr;
+      }
+      auto ref arr()() if(isArray!Type) {
+        auto len = size / ElementType!Type.sizeof;
+        return val[0..len];
+      }
+      auto toString() const => ptr.to!string;
+    }
+
+    static if(isDyns!Type)  return _dyns_dynses[i].tryMatch!((Type d) => d);
+    else                    return Field(this);
   }
 }
 
 unittest {
-  struct S {
+  alias Sdyn = Dyns!(
+    size_t,   "n",
+    ubyte[0], "buf");
+
+  struct Sbuf {
+    size_t    n;
+    ubyte[4]  buf;
+  }
+
+  {
+    auto buf = Sbuf(4, [1, 2, 3, 4]);
+    auto s = new Sdyn(&buf);
+    s.buf.size = s.n.val;
+    assert(s.size == 12);
+    s.n.val = 123;
+    assert(s.n.val == 123);
+    s.buf.arr[2] = 5;
+    assert(s.buf.arr == [1, 2, 5, 4]);
+  }
+
+  struct S2 {
     ubyte     buf1;
     ubyte[0]  buf2;
-    ushort    buf3;
-    ubyte[0]  buf4;
+    ushort[2] buf3;
+    Sdyn      buf4;
     size_t    buf5;
   }
-  const Sd = Dyns.from!S;
 
-  auto d = new Dyns(Sd);
-  d.buf2.size = 1;
-  d.buf4.size = 4;
-  d.alloc;
-  assert(d.size == 16);
-
-  d.buf5.val!size_t = 123;
-  assert(d.buf5.val!size_t == 123);
+  {
+    auto s = new Dyns!S2;
+    s.buf2.size = 3;
+    s.buf4.buf.size = 4;
+    assert(s.size == 28);
+    s.alloc;
+    s.buf5.val = size_t.max;
+    assert(s.buf5.val == size_t.max);
+  }
 }
