54264ec7c98cc6e8eb1b4c8fd69438e2e4f4557e
YamaD <me@dyama.net>
2025-01-22 14:32:19 +0900

    update utils.hh

--- cxx/utils.hh
+++ cxx/utils.hh
@@ -1,5 +1,21 @@
 #pragma once
 
-[[noreturn]] inline auto unreachable() {
-  __builtin_unreachable();
-}
+#include <utility>
+
+#define _JOIN(a, b) a##b
+#define JOIN(a, b) _JOIN(a, b)
+
+#define init(code) [[maybe_unused]]                          \
+                   static auto JOIN(_init_, __LINE__) = [] { \
+                     [] { code; }();                         \
+                     return 0;                               \
+                   }();
+
+#define defer(code) [[maybe_unused]] Defer JOIN(_defer_, __LINE__){[&] { code }};
+
+template <class F>
+struct Defer {
+  F f;
+  Defer(F&& f) : f{std::forward<F>(f)} {}
+  ~Defer() { f(); }
+};
